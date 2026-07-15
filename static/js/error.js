5ff82de8439ae5eee0e53307ae86cdf59b781f74
xangeyfun <kutkindje101@gmail.com>
2026-07-16 00:27:45 +0200

    remove dead error.js file

--- static/js/error.js
+++
@@ -1,6 +0,0 @@
-fetch("https://naas.isalman.dev/no")
-.then(r => r.json())
-.then(d => new Promise(resolve => setTimeout(() => resolve(d), 1000)))
-.then(d => {
-    document.getElementById("reason").textContent = d.reason;
-});
