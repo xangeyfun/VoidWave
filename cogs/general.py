84033b6f25e1e9d1b5c99022805e77834c47f90e
xangeyfun <kutkindje101@gmail.com>
2026-08-20 20:35:17 +0200

    Add another voting link

--- cogs/general.py
+++ cogs/general.py
@@ -151,6 +151,7 @@ class GeneralCog(commands.Cog):
             f"> GitHub: <https://github.com/xangeyfun/VoidWave>\n"
             f"> Website: <https://voidwave.xangey.dev/>\n"
             f"> Vote on Top.gg: <https://top.gg/bot/1442229230384709752/vote>\n"
+            f"> Vote on Discord List: <https://discordlist.gg/bot/1442229230384709752>\n"
             f"> Vote on DiscordBotList: <https://discordbotlist.com/bots/voidwave/upvote>",
             ephemeral=True,
             allowed_mentions=discord.AllowedMentions(users=False)
@@ -196,6 +197,7 @@ class GeneralCog(commands.Cog):
             name="Vote Links",
             value=(
                 "> <https://top.gg/bot/1442229230384709752/vote>\n"
+                "> <https://discordlist.gg/bot/1442229230384709752>\n"
                 "> <https://discordbotlist.com/bots/voidwave/upvote>"
             ),
             inline=False,
